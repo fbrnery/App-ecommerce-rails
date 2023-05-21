e17225f4a41f6f79f0cfd34b60d48437ef6a2466
Fabricio Miranda <fbrnery@gmail.com>
2023-05-21 16:39:14 -0300

    Adding a column to an existing table, with a default value

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -27,7 +27,7 @@ class LineItemsController < ApplicationController
   # POST /line_items.json
   def create
     product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product)
+    @line_item = @cart.add_product(product)
 
     respond_to do |format|
       if @line_item.save
@@ -48,8 +48,9 @@ class LineItemsController < ApplicationController
   def update
     respond_to do |format|
       if @line_item.update(line_item_params)
-        format.html { redirect_to @line_item, notice: 'Line item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @line_item }
+        format.html { redirect_to @line_item.cart }
+        format.json { render :show,
+          status: :created, location: @line_item }
       else
         format.html { render :edit }
         format.json { render json: @line_item.errors, status: :unprocessable_entity }
@@ -75,7 +76,7 @@ class LineItemsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def line_item_params
-      params.require(:line_item).permit(:product_id, :cart_id)
+      params.require(:line_item).permit(:product_id)
     end
   #...
 end
